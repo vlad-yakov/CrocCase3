3a5b8f0bba2451f9b8ce29856aec94bc0ff9ead2
Vladislav <79140944+vlad-yakov@users.noreply.github.com>
2021-10-21 22:01:50 +0400

    Add files via upload

---
+++ CrocCase3/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <schedule msg="Calendar"/>
+</template>
+
+<script>
+import schedule from './components/Schedule.vue'
+
+export default {
+  name: 'App',
+  components: {
+    schedule
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
