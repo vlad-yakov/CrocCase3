7f3102a51f8295af9820675b06f8ca40d2075195
Vladislav <79140944+vlad-yakov@users.noreply.github.com>
2021-11-12 11:30:28 +0400

    шаблон календаря без юзабилити

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <div class="d-flex align-center">
+        <v-img />
+
+        <v-img
+          alt="croc-name"
+          contain
+          min-width="100"
+          src=".\assets\croc-name.svg"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="" target="_blank">
+        <v-icon> mdi-magnify color="black" </v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <graph />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import graph from "./components/graph";
+export default {
+  name: "App",
+  data: () => ({}),
+  components: {
+    graph,
+  },
+};
+</script>
