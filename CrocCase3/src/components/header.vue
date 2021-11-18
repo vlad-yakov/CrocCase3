79c870b00a355a4f5fc457056492f014ce4d5369
Vladislav <79140944+vlad-yakov@users.noreply.github.com>
2021-11-18 23:41:04 +0400

    page calendar+header

---
+++ CrocCase3/src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app>
+    <div class="d-flex align-center">
+      <v-img
+        alt="croc-name"
+        contain
+        min-width="100"
+        src="..\assets\croc-name.svg"
+        width="100"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="" target="_blank">
+      <v-icon> mdi-magnify color="black" </v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.component("header", {
+  data: () => ({
+    projectName: "Здесь имя проекта",
+  }),
+});
+</script>
