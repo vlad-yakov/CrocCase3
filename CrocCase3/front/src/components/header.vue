4a08fabafd76022e5b3b876ee8b35177a291cf6b
Vladislav <79140944+vlad-yakov@users.noreply.github.com>
2021-11-29 18:27:53 +0400

    основная страница+ поиск проекта

---
+++ CrocCase3/front/src/components/header.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <v-app-bar app flat>
+      <div class="d-flex align-center">
+        <v-img
+          alt="croc-name"
+          contain
+          min-width="100"
+          src="..\assets\croc-name.svg"
+          width="100"
+        />
+      </div>
+
+      <v-toolbar-title class="p_name">
+        {{ title }}
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="" target="_blank" color="#00A460">
+        <v-icon color="white"> mdi-file-plus-outline </v-icon>
+      </v-btn>
+
+      <v-btn href="" target="_blank" color="#00A460">
+        <v-icon color="white"> mdi-note-edit-outline </v-icon>
+      </v-btn>
+      <v-search></v-search>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import search from "./search";
+
+export default Vue.component("v-header", {
+  components: search,
+
+  props: ["title"],
+  data: () => ({
+    "--castSize": null,
+    search: false,
+  }),
+  computed: {
+    //динамически изменяет размер шрифта
+    "--castSize": function () {
+      if (this.title.length > 74) {
+        return 10;
+      } else if (this.title.length > 148) {
+        return 5;
+      }
+      return 20;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@font-face {
+  font-family: "CrocBold";
+  src: url("../fonts/CrocBold.eot");
+  src: url("../fonts/CrocBold.eot") format("embedded-opentype"),
+    url("../fonts/CrocBold.svg#CrocBold") format("svg"),
+    url("../fonts/CrocBold.ttf") format("truetype");
+}
+
+.v-btn {
+  margin: 5px;
+}
+
+.p_name {
+  margin: 30px;
+  max-height: 40px;
+  max-width: 800px;
+  white-space: pre-wrap;
+  font-size: 20px;
+  font-size: calc(var(--castSize) * 1px);
+}
+
+.v-toolbar {
+  font-family: CrocBold;
+  border-bottom: 3px solid #00a460;
+}
+</style>
---
+++ CrocCase3/front/src/components/search.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="search">
+    <template>
+      <v-btn class="mrg" color="#00A460" dark @click.stop="search = !search">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </template>
+
+    <v-contaniner>
+      <v-navigation-drawer
+        app
+        fixed
+        floating
+        v-model="search"
+        absolute
+        temporary
+        right="true"
+        width="440px"
+        overlay-opacity="0"
+      >
+        <v-sheet>
+          <v-row>
+            <v-col cols="11"></v-col>
+            <v-col cols="12" sm="9" offset="0px">
+              <v-text-field
+                dense
+                v-model="find"
+                clearable
+                label="Найти проект"
+                color="#00A460"
+                id="poisk"
+              ></v-text-field
+              ><!--rules принимает аргументы --></v-col
+            >
+
+            <v-btn dark color="#00A460"> Найти </v-btn>
+          </v-row>
+        </v-sheet>
+        <h3>Мои проекты:</h3>
+        <div class="myProjects">
+          <v-list-item v-for="pr in Projects" :key="pr.token"> </v-list-item
+          ><!--нужно правильно назначить свойство объекта по которому итерируется массив -->
+          <v-list-item-content>
+            <v-list-item-title>Single-line item</v-list-item-title>
+          </v-list-item-content>
+        </div>
+        <hr color="#00A460" />
+        <div class="myProjects">
+          <v-list-item v-for="pr in project" :key="pr.id"> </v-list-item>
+          <!--нужно правильно назначить свойство объекта по которому итерируется массив -->
+          <v-list-item-content>
+            <v-list-item-title>Single-line item</v-list-item-title>
+          </v-list-item-content>
+        </div>
+      </v-navigation-drawer></v-contaniner
+    >
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.component("v-search", {
+  props: ["names", "project"],
+  data() {
+    return {
+      search: false,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.mrg {
+  margin: 5px;
+}
+
+.v-navigation-drawer {
+  padding: 10px;
+}
+
+.v-text-field {
+  margin: 15px;
+}
+
+.myProjects {
+  margin: 5px 0px 5px 30px;
+}
+
+hr {
+  margin: 15px;
+}
+h3 {
+  padding: 7px 15px;
+}
+</style>
