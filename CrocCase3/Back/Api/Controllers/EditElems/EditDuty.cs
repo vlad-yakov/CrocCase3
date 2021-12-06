949e9e85dbb7c50994298c01d293045220099d50
Anton <antonshabanowd@gmail.com>
2021-12-06 04:35:41 +0300

    Добавил контроллеры для изменения проекта и смены.

--- CrocCase3/Back/Api/Controllers/EditElems/EditDuty.cs
+++ CrocCase3/Back/Api/Controllers/EditElems/EditDuty.cs
@@ -1,52 +1,52 @@
 ﻿using System;
 using Api.Controllers.Authorize;
 using Api.Models;
-using DataModel.Models;
 using DataModel.Models.Duty;
-using DataModel.Models.User;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Services.UseCases.AddElem;
+using Services.UseCases.EditElem;
 
 namespace Api.Controllers.AddElems
 {
     /// <summary>
-    /// Контроллер, добавляющий смену для пользователя в таблицу.
+    /// Контроллер, изменяющий смену для пользователя в таблице.
     /// </summary>
     [ApiController]
-    [Route("AddDuty")]
-    public class AddDutyController : ControllerBase
+    [Route("EditDuty")]
+    public class EditDutyController : ControllerBase
     {
         /// <summary>
         /// Получить ответ от сервера.
         /// </summary>
+        /// <param name="id">Идентификатор смены.</param>
         /// <param name="start">Имя пользователя.</param>
         /// <param name="end">Почта для связи с данным пользователем.</param>
         /// <param name="linkerId">Телефон для связи с данным пользователем.</param>
         /// <param name="token">Токен пользователя.</param>
         /// <returns>Ответ сервера с информацией о результативности выполнения задания.</returns>
         [HttpGet]
-        public ResultMessage<int> Get(DateTime start, DateTime end, int linkerId, string token)
+        public SuccessMessage Get(int id, DateTime start, DateTime end, int linkerId, string token)
         {
             var duty = new DutyModel
             {
+                Id = id,
                 Start = start,
                 Finish = end,
                 LinkerId = linkerId
             };
             
-            var result = new ResultMessage<int>();
+            var result = new SuccessMessage();
             try
             {
                 var userLogin = new TokenOperations().CheckToken(token);
-                var dutyAddService = new AddDuty();
-                result.Result = dutyAddService.TryExecute(duty, userLogin);
-                result.Success.Success = true;
+                var dutyAddService = new EditDuty();
+                dutyAddService.TryExecute(duty, userLogin);
+                result.Success = true;
             }
             catch (Exception e)
             {
-                result.Success.Success = false;
-                result.Success.Reason.Add(e.Message);
+                result.Success = false;
+                result.Reason.Add(e.Message);
                 return result;
             }
 
---
+++ CrocCase3/Back/Services/UseCases/EditElem/EditDuty.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using DataModel;
+using DataModel.Models.Duty;
+using Microsoft.EntityFrameworkCore;
+using Services.UseCases.AuthorizeAndCheckPermissions;
+
+namespace Services.UseCases.EditElem
+{
+    /// <summary>
+    /// Редактирует смену в базе данных.
+    /// </summary>
+    public class EditDuty
+    {
+        /// <summary>
+        /// Выполнить действие, подразумеваемое в описании Обьекта.
+        /// </summary>
+        /// <param name="dutyInput">Информация о смене.</param>
+        /// <param name="login">Логин пользователя.</param>
+        /// <returns>Идентификатор добавленного элемента.</returns>
+        public void TryExecute(DutyModel dutyInput, string login)
+        {
+            if (dutyInput.Start == null)
+                throw new UseCaseException("Дата начала смены не может быть пустой.");
+            
+            if (dutyInput.Finish == null)
+                throw new UseCaseException("Дата конца смены не может быть пустой.");
+            
+            if (dutyInput.LinkerId <= 0)
+                throw new UseCaseException("Идентификатор линкера не может быть пустым.");
+
+            int projectId;
+            using (var db = new DataContext())
+            {
+                projectId = db.Linker.FirstOrDefault(linker => linker.Id == dutyInput.LinkerId).ProjectId;
+            }
+            
+            string userLogin;
+            using (var db = new DataContext())
+            {
+                var userLoginObj = db.Linker
+                    .Where(linker => linker.Id == dutyInput.LinkerId)
+                    .Include(linker => linker.User)
+                    .FirstOrDefault();
+
+                if (userLoginObj == null)
+                    throw new UseCaseException("Недостаточно прав.");
+
+                userLogin = userLoginObj.User.Login;
+            }
+            
+            var sysAdmin = new CheckSystemAdminByLogin().TryExecute(login);
+            var projectAdmin = new CheckCurrentProjectAdminByLogin().TryExecute(login, projectId);
+            
+            if (!(sysAdmin || projectAdmin) || userLogin != login)
+                throw new UseCaseException("Недостаточно прав.");
+            
+            using (var db = new DataContext())
+            {
+                var addedElem = db.Duties.Update(dutyInput);
+                db.SaveChanges();
+            }
+        }
+    }
+}
